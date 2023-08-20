518f2d9b83fee0f9a162682b79b3ea5b291337dd
Guilherme Mota <guilherme.mota@hotamil.com>
2023-08-20 19:57:34 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03f65b8614a860c29"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"  
+  tags = {
+    Name = "primeira Instancia"
+  }
+}
