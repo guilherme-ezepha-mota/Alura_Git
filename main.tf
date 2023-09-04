32fe7872fbcc63dde1a86d3be0532827f25bb32b
Guilherme Mota <guilherme.mota@hotamil.com>
2023-09-03 22:13:32 -0300

    Adicionando user_data no main

--- main.tf
+++ main.tf
@@ -17,7 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
   key_name = "iac-alura"  
+  user_data = <<-EOF
+                  #!/bin/bash
+                  sudo apt-get update
+                  cd /home/ubuntu
+                  echo "<h1> Feito com Terraform</h1>" > index.html
+                  nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
-    Name = "primeira Instancia"
+    Name = "Teste AWS"
   }
 }
